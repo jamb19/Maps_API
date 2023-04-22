8d43ffffdb963a0d8f7f1861dad2f7694d87d9db
Jesus19 <martinezba191297@gmail.com>
2023-04-22 17:35:09 -0600

    Aplicación de Google Maps en Android con Kotlin Hecho por: Jesus Alberto Martinez Barragan

---
+++ app/src/main/java/com/example/maps/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.maps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createMapFragment()
+    }
+
+    private fun createMapFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        val favoritePlace = LatLng(17.01900070980514, -96.76415808909056)
+        map.addMarker(MarkerOptions().position(favoritePlace).title("Mi Universidad"))
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(favoritePlace, 18f),
+            4000,
+            null
+        )
+    }
+
+}
